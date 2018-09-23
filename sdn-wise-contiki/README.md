bb55e365b75816f0f615fc8e19cb88d7813300c0
Andre Felippe Weber <andrefelippe.weber@gmail.com>
2018-09-23 14:30:58 +0200

    Initial Commit: Adding Controller and Contiki nodes code

---
+++ sdn-wise-contiki/README.md
@@ -0,0 +1,58 @@
+SDN-WISE-CONTIKI
+====================================
+[![Build Status](https://travis-ci.org/sdnwiselab/sdn-wise-contiki.svg?branch=master)](https://travis-ci.org/sdnwiselab/sdn-wise-contiki)
+[![Analytics](https://ga-beacon.appspot.com/UA-112624043-2/sdn-wise-contiki/readme)](https://github.com/sdnwiselab/sdn-wise-contiki)
+
+The Open Source OS for the Internet of Things meets the Software Defined Networking.
+
+### Build
+
+To build SDN-WISE run the `sdn-wise/RUNME.sh` script.
+
+To select the target for your build change the `TARGET` variable inside this script with one of the supported contiki [platforms](http://www.contiki-os.org/hardware.html).
+
+To build SDN-WISE for Cooja emulated devices please set `COOJA_BUILD=1` otherwise set `COOJA_BUILD=0`.
+
+The script will deploy two compiled firmwares inside `sdn-wise/cooja_firmware`. One for the sink of the network, called `sink.target`, and one for the other nodes, called `node.target`.
+
+
+### Versioning
+
+This project uses [semantic versioning](http://semver.org).
+
+## Papers
+
+Our approach is detailed in three scientific contributions:
+```
+@inproceedings{DiDio:2016,
+  author    = {Paolo {Di Dio} and Salvatore Faraci and Laura Galluccio and Sebastiano Milardo and Giacomo Morabito and Sergio Palazzo and Patrizia Livreri},
+  booktitle = {2016 Mediterranean Ad Hoc Networking Workshop (Med-Hoc-Net)},
+  doi       = {10.1109/MedHocNet.2016.7528421},
+  title     = {{Exploiting state information to support QoS in Software-Defined WSNs}},
+  year      = {2016},
+  url       = {http://ieeexplore.ieee.org/document/7528421/},
+}
+```
+
+```
+@inproceedings{Galluccio:2015b,
+  author    = {Laura Galluccio and Sebastiano Milardo and Giacomo Morabito and Sergio Palazzo},
+  booktitle = {2015 IEEE Conference on Computer Communications Workshops (INFOCOM WKSHPS)},
+  doi       = {10.1109/INFCOMW.2015.7179322},
+  title     = {{Reprogramming Wireless Sensor Networks by using SDN-WISE: A hands-on demo}},
+  year      = {2015},
+  url       = {http://ieeexplore.ieee.org/document/7179322/},
+}
+```
+
+```
+@inproceedings{Galluccio:2015a,
+  author    = {Laura Galluccio and Sebastiano Milardo and Giacomo Morabito and Sergio Palazzo},
+  booktitle = {2015 IEEE Conference on Computer Communications (INFOCOM)},
+  doi       = {10.1109/INFOCOM.2015.7218418},
+  title     = {{SDN-WISE: Design, prototyping and experimentation of a stateful SDN solution for WIreless SEnsor networks}},
+  year      = {2015},
+  url       = {http://ieeexplore.ieee.org/document/7218418/},
+}
+```
+
---
+++ sdn-wise-contiki/sdn-wise/sdn-wise.c
@@ -0,0 +1,452 @@
+/*
+ * Copyright (C) 2015 SDN-WISE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file
+ *         SDN-WISE for Contiki.
+ * \author
+ *         Sebastiano Milardo <s.milardo@hotmail.it>
+ */
+
+/**
+ * \addtogroup sdn-wise
+ * @{
+ */
+
+#include "contiki.h"
+#include "net/rime/rime.h"
+#include "net/linkaddr.h"  
+#include "dev/watchdog.h"
+#include "dev/uart1.h"
+#include "dev/leds.h"  
+#include "lib/list.h"
+#if CFS_ENABLED
+#include "cfs/cfs.h"
+#endif
+#if ELF_ENABLED
+#include "loader/elfloader.h"
+#endif
+#include "sdn-wise.h"
+#include "flowtable.h"
+#include "packet-buffer.h"
+#include "packet-handler.h"
+#include "packet-creator.h"
+#include "neighbor-table.h"
+#include "node-conf.h"
+
+#include "contiki-net.h"
+#include "sys/cc.h"
+
+#define UART_BUFFER_SIZE      MAX_PACKET_LENGTH
+
+#define UNICAST_CONNECTION_NUMBER     29
+#define BROADCAST_CONNECTION_NUMBER       30
+
+#define TIMER_EVENT           50
+#define RF_U_SEND_EVENT       51
+#define RF_B_SEND_EVENT       52
+#define RF_U_RECEIVE_EVENT    53
+#define RF_B_RECEIVE_EVENT    54
+#define UART_RECEIVE_EVENT    55
+#define RF_SEND_BEACON_EVENT  56
+#define RF_SEND_REPORT_EVENT  57
+#define RF_SEND_DATA_EVENT    58
+#define NEW_PACKET_EVENT      59
+#define ACTIVATE_EVENT        60
+
+#ifndef SDN_WISE_DEBUG
+#define SDN_WISE_DEBUG 0
+#endif
+#if SDN_WISE_DEBUG
+#include <stdio.h>
+#define PRINTF(...) printf(__VA_ARGS__)
+#else
+#define PRINTF(...)
+#endif
+
+#define CONTROLLER_PORT 9991
+#define CONTROLLER_ADDR "127.0.0.0"
+
+/*----------------------------------------------------------------------------*/
+  PROCESS(main_proc, "Main Process");
+  PROCESS(rf_u_send_proc, "RF Unicast Send Process");
+  PROCESS(rf_b_send_proc, "RF Broadcast Send Process");
+  PROCESS(packet_handler_proc, "Packet Handler Process");
+  PROCESS(timer_proc, "Timer Process");
+  PROCESS(beacon_timer_proc, "Beacon Timer Process");
+  PROCESS(report_timer_proc, "Report Timer Process");
+  PROCESS(data_timer_proc, "Data Timer Process");
+  PROCESS(tcp_server_process, "TCP echo process");
+
+  AUTOSTART_PROCESSES(
+    &main_proc,
+    &rf_u_send_proc,
+    &rf_b_send_proc,
+    &timer_proc,
+    &beacon_timer_proc,
+    &report_timer_proc,
+    &packet_handler_proc,
+    &data_timer_proc,
+    &tcp_server_process
+  );
+/*----------------------------------------------------------------------------*/
+static struct tcp_socket socket;
+#define INPUTBUFSIZE 400
+static uint8_t inputbuf[INPUTBUFSIZE];
+#define OUTPUTBUFSIZE 400
+static uint8_t outputbuf[OUTPUTBUFSIZE];
+uip_ip4addr_t ip4addr;
+
+  static uint8_t uart_buffer[UART_BUFFER_SIZE];
+  static uint8_t uart_buffer_index = 0;
+  static uint8_t uart_buffer_expected = 0;
+#if MOBILE
+  static uint8_t count=0;
+  static packet_t* p;
+#endif
+/*----------------------------------------------------------------------------*/
+  void
+  rf_unicast_send(packet_t* p)
+  {
+    process_post(&rf_u_send_proc, RF_U_SEND_EVENT, (process_data_t)p);
+  }
+/*----------------------------------------------------------------------------*/
+  void
+  rf_broadcast_send(packet_t* p)
+  {
+    process_post(&rf_b_send_proc, RF_B_SEND_EVENT, (process_data_t)p);
+  }
+
+/*----------------------------------------------------------------------------*/
+  static uint8_t
+  get_packet_rssi(uint16_t raw_value)
+  {
+    // TODO the exact rssi value depends on the radio
+    // http://sourceforge.net/p/contiki/mailman/message/31805752/
+#if COOJA
+    return (uint8_t) -raw_value;
+#else
+    return (uint8_t) raw_value;
+#endif
+  }
+
+/*----------------------------------------------------------------------------*/
+  static void
+  unicast_rx_callback(struct unicast_conn *c, const linkaddr_t *from)
+  {
+    packet_t* p = get_packet_from_array((uint8_t *)packetbuf_dataptr());
+    if (p != NULL){
+      p->info.rssi = get_packet_rssi(packetbuf_attr(PACKETBUF_ATTR_RSSI));
+      process_post(&main_proc, RF_U_RECEIVE_EVENT, (process_data_t)p);
+    }
+  }
+/*----------------------------------------------------------------------------*/
+  static void
+  broadcast_rx_callback(struct broadcast_conn *c, const linkaddr_t *from)
+  {
+    packet_t* p = get_packet_from_array((uint8_t *)packetbuf_dataptr());
+    if (p != NULL){
+      p->info.rssi = get_packet_rssi(packetbuf_attr(PACKETBUF_ATTR_RSSI));
+      process_post(&main_proc, RF_B_RECEIVE_EVENT, (process_data_t)p);
+    }
+  }
+/*----------------------------------------------------------------------------*/
+  int
+  uart_rx_callback(unsigned char c)
+  {
+    uart_buffer[uart_buffer_index] = c;
+    if (uart_buffer_index == LEN_INDEX){
+      uart_buffer_expected = c;
+    }
+    uart_buffer_index++;
+    if (uart_buffer_index == uart_buffer_expected){
+      uart_buffer_index = 0;
+      uart_buffer_expected = 0;
+      packet_t* p = get_packet_from_array(uart_buffer);
+      if (p != NULL){
+        p->info.rssi = 0;
+        process_post(&main_proc, UART_RECEIVE_EVENT, (process_data_t)p);  
+      }
+    }
+    return 0;
+  }
+/*----------------------------------------------------------------------------*/
+ static const struct unicast_callbacks unicast_callbacks = {
+    unicast_rx_callback
+  };
+  static struct unicast_conn uc;
+  static const struct broadcast_callbacks broadcast_callbacks = {
+    broadcast_rx_callback
+  };
+  static struct broadcast_conn bc;
+/*----------------------------------------------------------------------------*/
+  PROCESS_THREAD(main_proc, ev, data) {
+    PROCESS_BEGIN();
+    
+    uart1_init(BAUD2UBR(115200));       /* set the baud rate as necessary */
+    uart1_set_input(uart_rx_callback);  /* set the callback function */
+
+    node_conf_init();
+    flowtable_init();
+    packet_buffer_init();
+    neighbor_table_init();
+    address_list_init();
+    leds_init();
+
+#if SINK
+    print_packet_uart(create_reg_proxy());
+#endif    
+
+    while(1) {
+      PROCESS_WAIT_EVENT();
+      switch(ev) {
+        case TIMER_EVENT:
+      // test_handle_open_path();
+      // test_flowtable();
+      // test_neighbor_table();
+      // test_packet_buffer();
+      // test_address_list();
+        print_flowtable();
+        print_node_conf();
+        break;
+
+        case UART_RECEIVE_EVENT:
+        leds_toggle(LEDS_GREEN);
+        process_post(&packet_handler_proc, NEW_PACKET_EVENT, (process_data_t)data);
+        break;
+
+        case RF_B_RECEIVE_EVENT:
+        leds_toggle(LEDS_YELLOW);
+        if (!conf.is_active){
+          conf.is_active = 1;
+          process_post(&beacon_timer_proc, ACTIVATE_EVENT, (process_data_t)NULL);
+          process_post(&report_timer_proc, ACTIVATE_EVENT, (process_data_t)NULL);
+        }
+        case RF_U_RECEIVE_EVENT:
+        process_post(&packet_handler_proc, NEW_PACKET_EVENT, (process_data_t)data);
+        break;
+
+        case RF_SEND_BEACON_EVENT:
+        rf_broadcast_send(create_beacon());
+        break;
+
+        case RF_SEND_REPORT_EVENT:
+        rf_unicast_send(create_report());
+        #if !SINK
+                if (conf.reset_period == 0){
+                  conf.distance_from_sink = _MAX_DISTANCE;
+                  conf.reset_period = _RESET_PERIOD;
+                } else {
+                  conf.reset_period--;
+                }
+        #endif
+        break;
+
+        case RF_SEND_DATA_EVENT:
+            #if MOBILE
+            if (conf.is_active){
+            p = create_data(count);
+                if (p != NULL){
+                    match_packet(p);
+                }
+            count++;
+               }
+       #endif
+        break;
+      } 
+    }
+    PROCESS_END();
+  }
+/*----------------------------------------------------------------------------*/
+  PROCESS_THREAD(rf_u_send_proc, ev, data) {
+  static linkaddr_t addr;
+    PROCESS_EXITHANDLER(unicast_close(&uc);)
+    PROCESS_BEGIN();
+
+    unicast_open(&uc, UNICAST_CONNECTION_NUMBER, &unicast_callbacks);
+    while(1) {
+      PROCESS_WAIT_EVENT_UNTIL(ev == RF_U_SEND_EVENT);
+      packet_t* p = (packet_t*)data;
+      if (p != NULL){
+        p->header.ttl--;
+
+        PRINTF("[TXU]: ");
+        print_packet(p);
+        PRINTF("\n");
+
+        if (!is_my_address(&(p->header.dst))){
+          int i = 0;
+
+          int sent_size = 0;
+
+          if (LINKADDR_SIZE < ADDRESS_LENGTH){
+            sent_size = LINKADDR_SIZE;
+          } else {
+            sent_size = ADDRESS_LENGTH;
+          }
+
+          for ( i=0; i<sent_size; ++i){
+            addr.u8[i] = p->header.nxh.u8[(sent_size-1)-i];
+          }
+
+          addr.u8[0] = p->header.nxh.u8[1];
+          addr.u8[1] = p->header.nxh.u8[0];
+          uint8_t* a = (uint8_t*)p;
+          packetbuf_copyfrom(a,p->header.len);
+          unicast_send(&uc, &addr);
+        }
+#if SINK
+        else {
+          print_packet_uart(p);
+        }
+#endif
+        packet_deallocate(p);
+      }
+    }
+    PROCESS_END();
+  }
+/*----------------------------------------------------------------------------*/
+  PROCESS_THREAD(rf_b_send_proc, ev, data) {
+    PROCESS_EXITHANDLER(broadcast_close(&bc);)
+    PROCESS_BEGIN();
+    broadcast_open(&bc, BROADCAST_CONNECTION_NUMBER, &broadcast_callbacks);
+    while(1) {
+
+      PROCESS_WAIT_EVENT_UNTIL(ev == RF_B_SEND_EVENT);
+      packet_t* p = (packet_t*)data;
+
+      if (p != NULL){
+        p->header.ttl--;
+        PRINTF("[TXB]: ");
+        print_packet(p);
+        PRINTF("\n");
+        
+        uint8_t* a = (uint8_t*)p;
+        packetbuf_copyfrom(a,p->header.len);
+        broadcast_send(&bc);
+        packet_deallocate(p);
+      }
+    }
+    PROCESS_END();
+  }
+/*----------------------------------------------------------------------------*/
+  PROCESS_THREAD(timer_proc, ev, data) {
+    static struct etimer et;
+    PROCESS_BEGIN();
+
+    while(1) {
+      etimer_set(&et, 3 * CLOCK_SECOND);
+      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+      process_post(&main_proc, TIMER_EVENT, (process_data_t)NULL);
+    }
+    PROCESS_END();
+  }
+/*----------------------------------------------------------------------------*/
+PROCESS_THREAD(beacon_timer_proc, ev, data) {
+    static struct etimer et;
+
+    PROCESS_BEGIN();
+    while(1){
+#if !SINK
+      if (!conf.is_active){
+        PROCESS_WAIT_EVENT_UNTIL(ev == ACTIVATE_EVENT);
+      }
+#endif
+      etimer_set(&et, conf.beacon_period * CLOCK_SECOND);
+      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+      process_post(&main_proc, RF_SEND_BEACON_EVENT, (process_data_t)NULL);
+    }
+    PROCESS_END();
+}
+/*----------------------------------------------------------------------------*/
+  PROCESS_THREAD(report_timer_proc, ev, data) {
+    static struct etimer et;
+    
+    PROCESS_BEGIN();
+    while(1){
+#if !SINK
+      if (!conf.is_active){
+        PROCESS_WAIT_EVENT_UNTIL(ev == ACTIVATE_EVENT);
+      }
+#endif
+      etimer_set(&et, conf.report_period * CLOCK_SECOND);
+      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+      process_post(&main_proc, RF_SEND_REPORT_EVENT, (process_data_t)NULL);
+    }
+    PROCESS_END();
+  }
+/*----------------------------------------------------------------------------*/
+  PROCESS_THREAD(data_timer_proc, ev, data) {
+    static struct etimer et;
+
+    PROCESS_BEGIN();
+    while(1){
+      etimer_set(&et, 5 * CLOCK_SECOND);
+      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+      process_post(&main_proc, RF_SEND_DATA_EVENT, (process_data_t)NULL);
+    }
+    PROCESS_END();
+  }
+
+/*----------------------------------------------------------------------------*/
+  PROCESS_THREAD(packet_handler_proc, ev, data) {
+    PROCESS_BEGIN();
+    while(1) {
+      PROCESS_WAIT_EVENT_UNTIL(ev == NEW_PACKET_EVENT);
+      packet_t* p = (packet_t*)data;
+      PRINTF("[RX ]: ");
+      print_packet(p);
+      PRINTF("\n");
+      int packet_type = handle_packet(p);
+      if(packet_type==2)
+	PRINTF("ENVIA PRO CONTROLLER CUZAO ");
+
+    }
+    PROCESS_END();
+  }
+/*--------------------------------------------------------------------------*/
+static int
+input(struct tcp_socket *s, void *ptr,
+      const uint8_t *inputptr, int inputdatalen)
+{
+  printf("input %d bytes '%s'\n", inputdatalen, inputptr);
+return 0;
+}
+/*---------------------------------------------------------------------------*/
+static void
+event(struct tcp_socket *s, void *ptr,
+      tcp_socket_event_t ev)
+{
+  printf("event %d\n", ev);
+}
+/*----------------------------------------------------------------------------*/
+PROCESS_THREAD(tcp_server_process, ev, data)
+{
+  PROCESS_BEGIN();
+        PRINTF(">>>>>>>>>>>>>process ");
+
+#if SINK
+        PRINTF("<<<<<<<<<<<<<<SINK \n");
+
+  tcp_socket_register(&socket, NULL, inputbuf, sizeof(inputbuf), outputbuf, sizeof(outputbuf), input, event);
+  //tcp_socket_listen(&socket, SERVER_PORT);
+  uip_ipaddr(&ip4addr, 127, 0, 0, 0);
+  tcp_socket_connect(&socket, &ip4addr, 9991 );
+#endif
+
+  PROCESS_END();
+}
